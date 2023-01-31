3f0c3ba95415caf5c52da956dd1de1bf1a9ac89e
vincenzoarena <penninoematita@gmail.com>
2023-01-31 03:32:28 +0100

    Building the login function WIP

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Login from './components/Login';
+
+function App() {
+  return (
+    // BEM
+    <div className="app">
+      
+      <Login />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/spotify.js
@@ -0,0 +1,15 @@
+// https://deeloper.spotify.com/documentation/web.playback-sdk/quick-start/#
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:3000/";
+const clientId = "94beb91e28e04a06a28983e2f9f9c04a";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
