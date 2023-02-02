6fa1aaa1402f121675b8ed7eaedaa591f53a7746
vincenzoarena <penninoematita@gmail.com>
2023-02-02 10:41:10 +0100

    Footer central and right parts WIP

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,8 @@ import { MdSkipPrevious } from '@react-icons/all-files/md/MdSkipPrevious';
 import { MdShuffle } from '@react-icons/all-files/md/MdShuffle';
 import { MdRepeat } from '@react-icons/all-files/md/MdRepeat';
 import { MdRepeatOne } from '@react-icons/all-files/md/MdRepeatOne';
+import { MdPlaylistPlay } from '@react-icons/all-files/md/MdPlaylistPlay';
+import { MdVolumeDown } from '@react-icons/all-files/md/MdVolumeDown';
 
 function Footer() {
   return (
@@ -15,14 +17,16 @@ function Footer() {
       </div>
 
       <div className="footer__center">
-        <MdShuffle className='footer__green' />
-        <MdSkipPrevious className='footer__icon' />
-        <MdPlayCircleOutline fontSize="xx-large" className='footer__icon' />
-        <MdSkipNext className='footer__icon' />
-        <MdRepeat className='footer__green' />
+        <MdShuffle className="footer__green" />
+        <MdSkipPrevious className="footer__icon" />
+        <MdPlayCircleOutline fontSize="xx-large" className="footer__icon" />
+        <MdSkipNext className="footer__icon" />
+        <MdRepeat className="footer__green" />
       </div>
 
       <div className="footer__right">
+        <MdPlaylistPlay />
+        <MdVolumeDown />
         <p>Volume Controls</p>
       </div>
     </div>
